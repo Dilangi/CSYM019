4294c783c49d1c555d8af0c5d0ecc045196f3c22
Dilangi <dilangithilakarathne@gmail.com>
2023-03-17 14:43:20 +0000

    Commit inclass week 5 activities

--- Week5/script.js
+++ Week5/script.js
@@ -1,3 +1,11 @@
+var count=0;
+var timer=0;
+
+function countOn(){
+    count = count+1;
+    console.log(count);
+}
+
 function changeBGColor(){
     var element = document.getElementById('circle');
     element.style.backgroundColor="blue";
@@ -28,15 +36,49 @@ function moveLeft(){
     element.style.left = element.offsetLeft -10+'px';
 }
 
+function moveRight(){
+    var element = document.getElementById('circle');
+    var positioLeft = element.offsetLeft;
+    element.style.left = element.offsetLeft +10+'px';
+}
+
+function moveUp(){
+    var element = document.getElementById('circle');
+    var positionTop = element.offsetTop;
+    element.style.top = element.offsetTop -10+'px';
+}
+
+function moveDown(){
+    var element = document.getElementById('circle');
+    var positionTop = element.offsetTop;
+    element.style.top = element.offsetTop +10+'px';
+}
+
 function triggerKey(event){
     console.log(event.keyCode);
+    
+    clearInterval(timer);
+
+    if(event.keyCode==37){
+        timer = setInterval(moveLeft,10);
+
+    }
+    else if(event.keyCode==38){
+        timer = setInterval(moveUp,10);
+    }
+    else if(event.keyCode==39){
+        timer = setInterval(moveRight,10);
+    }
+    else if(event.keyCode==40){
+        timer = setInterval(moveDown,10);
+    }
 }
 
 function loadAll(){
     var element = document.getElementById('circle');
     // element.style.opacity="0.5";
     element.style.opacity="1";
-    // element.addEventListener('click', moveLeft);
+    // element.addEventListener('click', countOn);
     document.addEventListener('keydown',triggerKey);
 }
 
